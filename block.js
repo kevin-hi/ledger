c272108d91e8ab8b2005a70af6373fa359948009
kouyang <kevin.ouyang@isobar.com>
2018-03-08 19:48:14 -0600

    Initial block implementation

--- block.js
+++ block.js
@@ -1,1 +1,12 @@
 "use strict";
+
+class Block {
+    constructor(hash, previousBlock, timestamp, transaction) {
+        this.hash = hash.toString();
+        this.previousBlock = previousBlock.toString();
+        this.timestamp = timestamp;
+        this.transaction = transaction;
+    }
+}
+
+module.exports = Block;
